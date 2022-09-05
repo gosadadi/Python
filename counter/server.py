e82a97e3e9d0d91c653f0c6dc79838f1b69abe78
gosa_dadi <gosa.d.dadi@gmail.com>
2022-09-04 17:35:39 -0700

    somemore work

--- counter/server.py
+++ counter/server.py
@@ -1,9 +1,25 @@
-from flask import Flask,render_template, redirect,request,session
-app=Flask(__name__)
-@app.route ('/')
+from flask import Flask, render_template, redirect, request,session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+
+@app.route('/')
 def counter():
-    return render_template ("counter.html")
+    if 'times' not in session:
+        session['times']=0
+    session['times']+=1
+    return render_template("counter.html", count=session['times'])
+
+@app.route('/users')
+def clicked():
+    session['times']+=1
+    return redirect('/')
+
+@ app.route('/reset')
+def reset():
+    session['times']=0
+    return redirect('/')
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
