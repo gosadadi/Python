8e81f1f189f11af8ade08a2502915d37ae16da47
gosa_dadi <gosa.d.dadi@gmail.com>
2022-09-15 09:42:27 -0700

    updated

--- user_recipe/flask_app/controllers/recipes.py
+++ user_recipe/flask_app/controllers/recipes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, request, session, flash
 from flask_app.models.recipe import Recipe
 from flask_app.models.user import User
 
+
 @app.route("/recipes")
 def home_page():
     if 'user_id' not in session:
@@ -10,14 +11,14 @@ def home_page():
     data = {
         "id": session['user_id']
     }
-    
+
     logged_user = User.get_one(data)
-    
-    return render_template("show.html", logged_user= logged_user, all_recipe=Recipe.get_all())
+
+    return render_template("show.html", logged_user=logged_user, all_recipe=Recipe.get_all())
 
 
 @app.route('/recipes/new')
-def recipes_new():
+def recipe_new():
 
     return render_template("recipe_form.html")
 
@@ -28,7 +29,7 @@ def create_recipes():
     if not is_valid:
         flash("please fill out all required form")
         return render_template("recipe_form.html")
-    
+
     data = {
         "name": request.form['name'],
         "description": request.form['description'],
@@ -42,39 +43,53 @@ def create_recipes():
     return redirect('/recipes')
 
 
-@app.route('/recipes/<int:recipe_id>')
-def view_one(recipe_id):
+@app.route('/recipes/<int:id>')
+def view_one(id):
 
     data = {
-        'id': recipe_id
+        'id': id
+    }
+    user= {
+        "id": session['user_id']
     }
 
-    return render_template("show_one_recipe.html", one_recipe=Recipe.get_one(data))
+    logged_user = User.get_one(user)
+
+    return render_template("show_one_recipe.html", one_recipe=Recipe.get_one(data), logged_user=User.get_one(user))
 
 
-@app.route('/update/recipe')
-def update_recipe():
-    
-    
-    return render_template("update_recipe_form.html") 
+@app.route('/edit_recipe/<int:id>')
+def edit_page(id):
+    data = {
+        'id': id
+    }
+    one_recipe = Recipe.get_one(data)
+    return render_template("update_recipe.html", one_recipe=one_recipe)
 
 
-@app.route('/update/<int:recipe_id>', methods=['POST'])
-def update(recipe_id):
+@app.route('/update/<int:id>', methods=['POST'])
+def update(id):
     is_valid = Recipe.validate_recipe(request.form)
     if not is_valid:
         flash("please fill out all required form")
         return render_template("recipe_form.html")
-    
-    
+
     data = {
-        'id': recipe_id,
+        'id': id,
         "name": request.form['name'],
         "description": request.form['description'],
         "instructions": request.form['instructions'],
-        "under_thirty":request.form["under_thirty"],
+        "under_thirty": request.form["under_thirty"],
         "user_id": session['user_id']
     }
     Recipe.update(data)
-    return redirect(f"/recipes/{recipe_id}")
+    return redirect("/recipes")
 
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    data = {
+        'id': id,
+    }
+    Recipe.destroy(data)
+    return redirect('/recipes')
--- user_recipe/flask_app/models/recipe.py
+++ user_recipe/flask_app/models/recipe.py
@@ -1,5 +1,7 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
+from flask_app.models.user import User
+
 
 class Recipe:
     def __init__(self, data):
@@ -11,13 +13,12 @@ class Recipe:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         self.user_id = data['user_id']
-        self.users = []
-
+        self.poster = None
 
     @classmethod
     def save(cls, data):
         query = "INSERT INTO recipes (name,description,instructions,under_thirty,created_at,updated_at,user_id) VALUES (%(name)s,%(description)s,%(instructions)s,%(under_thirty)s,%(created_at)s,NOW(),%(user_id)s);"
-        recipe_id= connectToMySQL('user_recipe').query_db(query, data)
+        recipe_id = connectToMySQL('user_recipe').query_db(query, data)
         return recipe_id
 
     @classmethod
@@ -27,25 +28,52 @@ class Recipe:
 
         recipes = []
         for recipe in results:
-            recipes.append(cls(recipe))
-
+            all_recipes = cls(recipe)
+            poster_data = {
+                'id': recipe['users.id'],
+                'first_name': recipe['first_name'],
+                'last_name': recipe['last_name'],
+                'email': recipe['email'],
+                'password': recipe['password'],
+                'created_at': recipe['created_at'],
+                'updated_at': recipe['updated_at']
+            }
+            all_recipes.poster = User(poster_data)
+            recipes.append(all_recipes)
         return recipes
 
-
     @classmethod
-    def get_one(cls,data):
+    def get_one(cls, data):
         query = "SELECT * FROM recipes LEFT JOIN users on users.id= recipes.user_id WHERE recipes.id = %(id)s;"
-        results = connectToMySQL('user_recipe').query_db(query,data)
+        results = connectToMySQL('user_recipe').query_db(query, data)
         if len(results) < 1:
             return False
-        return cls(results[0])
+        one_instance = cls(results[0])
+        one_data = {
+            'id': results[0]['users.id'],
+            'first_name': results[0]['first_name'],
+            'last_name': results[0]['last_name'],
+            'email': results[0]['email'],
+            'password': results[0]['password'],
+            'created_at': results[0]['created_at'],
+            'updated_at': results[0]['updated_at']
+        }
+        one_instance.poster = User(one_data)
 
+        return one_instance
 
     @classmethod
-    def update(cls,data):
-        query = "UPDATE recipes SET name,description, instructions,under_thirty=%(name)s,%(description)s,%(instructions)s,%(under_thirty)s,updated_at=NOW() WHERE id = %(recipe_id)s;"
-        return connectToMySQL('user_recipe').query_db(query,data)
+    def update(cls, data):
+        query = "UPDATE recipes SET name=%(name)s,description=%(description)s,instructions=%(instructions)s, under_thirty=%(under_thirty)s,updated_at = NOW(),user_id=%(user_id)s WHERE id = %(id)s;"
+        return connectToMySQL('user_recipe').query_db(query, data)
 
+        # query = "UPDATE recipes SET name,description, instructions,under_thirty,update_at,user_id=%(name)s,%(description)s,%(instructions)s,%(under_thirty)s,NOW(),%(user_id)s WHERE recipes.id = %(id)s;"
+        # return connectToMySQL('user_recipe').query_db(query, data)
+
+    @classmethod
+    def destroy(cls, data):
+        query = "DELETE FROM recipes WHERE id = %(id)s;"
+        return connectToMySQL('user_recipe').query_db(query, data)
 
     @staticmethod
     def validate_recipe(recipe):
@@ -60,15 +88,3 @@ class Recipe:
             is_valid = False
             flash("instructions must be at least 2 characters.", "register")
         return is_valid
-
-
-
-
-
-
-
-
-
-
-
-
